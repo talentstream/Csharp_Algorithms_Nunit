bc13c5d1becabc734416897452fdc11c2206fd7f
talentstream <447721326@qq.com>
2023-01-31 22:06:52 +0800

    1

--- Algorithms/Sorters/BubbleSorter.cs
+++ Algorithms/Sorters/BubbleSorter.cs
@@ -13,16 +13,23 @@ namespace Algorithms.Sorters
     public class BubbleSorter<T> : IComparisonSorter<T>
     {
         /// <summary>
-        ///     
+        ///     时间复杂度 O(n^2)
+        ///     空间复杂度 O(1)
+        ///     在这里，我们按照升序排序
+        ///     比较相邻元素，大的放在后面
+        ///     这样子每次最后一个是最大元素
+        ///     在不包含最大元素的范围重复步骤，直到没得交换
+        ///     最好情况是 N - 1 次交换和 0 次交换
+        ///     最坏情况是 N * (N - 1)/2 次交换
         /// </summary>
-        /// <param name="array"></param>
-        /// <param name="comparer"></param>
+        /// <param name="array">要排序的数组</param>
+        /// <param name="comparer">比较器</param>
         public void Sort(T[] array, IComparer<T> comparer)
         {
-            for(var i = 0;i< array.Length-1; i++)
+            for (var i = 0; i < array.Length - 1; i++)
             {
                 var wasChanged = false;
-                for(var j = 0; j < array.Length - i - 1; j++)
+                for (var j = 0; j < array.Length - i - 1; j++)
                 {
                     if (comparer.Compare(array[j], array[j + 1]) > 0)
                     {
@@ -32,6 +39,7 @@ namespace Algorithms.Sorters
                         wasChanged = true;
                     }
                 }
+
                 if (!wasChanged)
                 {
                     break;
---
+++ Algorithms/Sorters/InsertionSorter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorters
+{
+    /// <summary>
+    ///     实现插入排序的算法
+    /// </summary>
+    /// <typeparam name="T">数组元素类型</typeparam>
+    public class InsertionSorter<T> : IComparisonSorter<T>
+    {
+        /// <summary>
+        ///     时间复杂度 O(n^2)
+        ///     空间复杂度 O(1)
+        ///     我们保证当前需要排序的元素的前面是有序的
+        ///     然后将该元素插入前面中
+        ///     由于前面是有序的，假设是升序
+        ///     只要找到第一个比它小的元素插入就停止
+        ///     否则就一直两两交换位置直到第一位
+        ///     平均情况下需要 N^2/4 次交换
+        ///     最坏情况需要 N^2/2 次交换
+        ///     最好情况需要 N - 1 次交换 和 0 次交换
+        /// </summary>
+        /// <param name="array">排序数组</param>
+        /// <param name="comparer">比较器</param>
+        public void Sort(T[] array, IComparer<T> comparer)
+        {
+            for (var i = 1; i < array.Length; i++)
+            {
+                for (var j = i; j > 0 && comparer.Compare(array[j], array[j - 1]) < 0; j--)
+                {
+                    var temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
