cefb106a66d74f7bd4560c484e117150f759f843
talentstream <447721326@qq.com>
2023-01-31 22:06:52 +0800

    2023/1/31

---
+++ Algorithms.Test/Sorters/InsertionSorterTests.cs
@@ -0,0 +1,29 @@
+﻿using Algorithms.Sorters;
+using Algorithms.Test.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Test.Sorters
+{
+    public static class InsertionSorterTests
+    {
+        [Test]
+        public static void ArraySorted([Random(0, 1000, 100, Distinct = true)] int n)
+        {
+            // Arrange
+            var sorter = new InsertionSorter<int>();
+            var intComparer = new IntComparer();
+            var (correctArray, testArray) = RandomHelper.GetArrays(n);
+
+            // Act
+            sorter.Sort(testArray, intComparer);// 前者是用自己写的算法进行比较
+            Array.Sort(correctArray, intComparer);// 后者是用系统自带的比较算法进行比较
+
+            // Assert
+            Assert.AreEqual(testArray, correctArray);
+        }
+    }
+}
---
+++ Algorithms/Sorters/SelectionSorter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorters
+{
+    /// <summary>
+    ///     实现选择排序的类
+    /// </summary>
+    /// <typeparam name="T">数组元素类型</typeparam>
+    public class SelectionSorter<T> : IComparisonSorter<T>
+    {
+        /// <summary>
+        ///     时间复杂度 O(n^2)
+        ///     空间复杂度 O(1)
+        ///     找到数组最小元素，然后和第一个位置交换
+        ///     指针向后移动一位，然后再在剩下的数组元素找出最小
+        ///     如此往复，总共进行了 N 次交换
+        /// </summary>
+        /// <param name="array">排序数组</param>
+        /// <param name="comparer">比较器</param>
+        public void Sort(T[] array, IComparer<T> comparer)
+        {
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                var jmin = i;
+                for(var j = i + 1; j < array.Length; j++)
+                {
+                    if(comparer.Compare(array[jmin], array[j]) > 0)
+                    {
+                        jmin = j;
+                    }
+                }
+                var temp = array[i];
+                array[i] = array[jmin];
+                array[jmin] = temp;
+            }
+        }
+    }
+}
